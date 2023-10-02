8ab454360bf9b4d2de5d88d9864343627a668832
Cornelius Ezeh <cornzeh@gmail.com>
2023-10-02 13:53:22 +0100

    finished created the names of each page

---
+++ src/main.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { store } from "./redux/store.js";
+import { Provider } from "react-redux";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query-devtools";
+import "./index.css";
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </Provider>
+  </React.StrictMode>
+);
